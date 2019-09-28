e259fa0810aca716ee926cdd5c38f157a01460ce
Riccardo Degan <riccardo.degan@inkwelldata.com>
2019-09-28 16:16:16 +0200

    Updates

---
+++ precache-manifest.9284cc6079f5c0720acf6418cd1d2e4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a880abc44d1b5492b4346783799dede7",
+    "url": "/weather_forecast/index.html"
+  },
+  {
+    "revision": "8a2cc246e95bbe5c8674",
+    "url": "/weather_forecast/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "33c4b064d6b382ac46b3",
+    "url": "/weather_forecast/static/js/2.d3961411.chunk.js"
+  },
+  {
+    "revision": "8a2cc246e95bbe5c8674",
+    "url": "/weather_forecast/static/js/main.49cb2c9d.chunk.js"
+  },
+  {
+    "revision": "53a4be58a0422008569b",
+    "url": "/weather_forecast/static/js/runtime-main.581d9b47.js"
+  }
+]);
